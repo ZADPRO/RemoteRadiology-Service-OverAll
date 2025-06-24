15d2b99443799d84f4bff2e52e196496c800f90a
gokulmzadroit <gokul.m@zadroit.com>
2025-06-24 15:50:21 +0530

    Creating Forgetpassword Service

---
+++ query/Authentication/ForgetPasswordQuery.go
@@ -0,0 +1,20 @@
+package query
+
+var UpdatePasswordSQL = `
+UPDATE userdomain."refAuthDomain"
+SET
+  "refADPassword" = ?,
+  "refADHashPass" = ?,
+  "refAHPassChangeStatus" = ?
+WHERE
+  "refUserId" = ?;
+`
+
+var UpdateUserDataSQL = `
+UPDATE
+  public."Users"
+SET
+  "refUserAgreementStatus" = ?
+WHERE
+  "refUserId" = ?
+`
