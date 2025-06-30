b6e49298841202cf2b5a6cdf8bd7d0291f85a6c3
gokulmzadroit <gokul.m@zadroit.com>
2025-06-30 17:40:42 +0530

    Adding API for Manage Appointment Service

--- routes/Appointment/ManageAppointmentRoutes.go
+++ routes/Appointment/ManageAppointmentRoutes.go
@@ -11,5 +11,7 @@ func InitManageAppointmentRoutes(router *gin.Engine) {
 	route := router.Group("/api/v1/manageappointment")
 	route.POST("/add", accesstoken.JWTMiddleware(), controllers.AddAppointmentController())
 	route.GET("/viewpatienthistory", accesstoken.JWTMiddleware(), controllers.ViewPatientHistory())
-	route.POST("/viewtechnicianpatientqueue", accesstoken.JWTMiddleware(), controllers.ViewTechnicianPatientQueue())
+	route.GET("/viewtechnicianpatientqueue", accesstoken.JWTMiddleware(), controllers.ViewTechnicianPatientQueue())
+	route.POST("/addAddtionalFiles", accesstoken.JWTMiddleware(), controllers.AddAddtionalFilesController())
+	route.POST("/viewAddtionalFiles", accesstoken.JWTMiddleware(), controllers.ViewAddtionalFilesController())
 }
