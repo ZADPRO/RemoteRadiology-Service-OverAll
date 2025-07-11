8b643a1617beb4bd39e71d1e2a68da4db7a336c7
gokulmzadroit <gokul.m@zadroit.com>
2025-07-11 12:46:47 +0530

    Update  the ReportIntakeForm

---
+++ internal/Model/Appointment/ReportIntakeFormModel.go
@@ -0,0 +1,181 @@
+package model
+
+import "time"
+
+type GetCheckAccessModel struct {
+	Status bool `json:"status" gorm:"column:status"`
+}
+
+type CheckAccessReq struct {
+	AppointmentId int `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+}
+
+type AssignGetReportReq struct {
+	AppointmentId  int  `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	PatientId      int  `json:"patientId" binding:"required" mapstructure:"patientId"`
+	ReadOnlyStatus bool `json:"readOnly" binding:"required" mapstructure:"readOnly"`
+}
+
+type AccessStatus struct {
+	Status                 bool `gorm:"column:status"`
+	RefAppointmentAccessId int  `gorm:"column:refAppointmentAccessId"`
+}
+
+type GetTechnicianIntakeData struct {
+	IntakeId   int    `json:"refTITFId" gorm:"column:refTITFId"`
+	QuestionId int    `json:"questionId" gorm:"column:refTITFQId"`
+	Answer     string `json:"answer" gorm:"column:refTITFAnswer"`
+}
+
+type GetReportIntakeData struct {
+	IntakeId   int    `json:"refRITFId" gorm:"column:refRITFId"`
+	QuestionId int    `json:"questionId" gorm:"column:refRITFQId"`
+	Answer     string `json:"answer" gorm:"column:refRITFAnswer"`
+}
+
+type GetReportTextContent struct {
+	IntakeId     int    `json:"refRTCId" gorm:"column:refRTCId"`
+	TextContent  string `json:"refRTCText" gorm:"column:refRTCText"`
+	RTSyncStatus bool   `json:"refRTSyncStatus" gorm:"column:refRTSyncStatus"`
+}
+
+type GetReportHistory struct {
+	RHId              int    `json:"refRHId" gorm:"column:refRHId"`
+	HandledUserId     int    `json:"refRHHandledUserId" gorm:"column:refRHHandledUserId"`
+	HandleStartTime   string `json:"refRHHandleStartTime" gorm:"column:refRHHandleStartTime"`
+	HandleEndTime     string `json:"refRHHandleEndTime" gorm:"column:refRHHandleEndTime"`
+	HandleUserName    string `json:"HandleUserName" gorm:"column:HandleUserName"`
+	HandleStatus      string `json:"refRHHandleStatus" gorm:"column:refRHHandleStatus"`
+	HandleContentText string `json:"refRHHandleContentText" gorm:"column:refRHHandleContentText"`
+}
+
+type GetReportComments struct {
+	RCId     int    `json:"refRCId" gorm:"column:refRCId"`
+	RCFor    int    `json:"refRCFor" gorm:"column:refRCFor"`
+	RCBy     int    `json:"refRCBy" gorm:"column:refRCBy"`
+	Status   string `json:"refRCStatus" gorm:"column:refRCStatus"`
+	Comments string `json:"refRCComments" gorm:"column:refRCComments"`
+	UserFor  string `json:"UserForName" gorm:"column:UserForName"`
+	UserBy   string `json:"UserByName" gorm:"column:UserByName"`
+}
+
+type AppointmentModel struct {
+	AppointmentId           int    `json:"refAppointmentId" gorm:"column:refAppointmentId"`
+	UserId                  int    `json:"refUserId" gorm:"column:refUserId"`
+	SCId                    int    `json:"refSCId" gorm:"column:refSCId"`
+	CategoryId              int    `json:"refCategoryId" gorm:"column:refCategoryId"`
+	AppointmentDate         string `json:"refAppointmentDate" gorm:"column:refAppointmentDate"`
+	AppointmentComplete     string `json:"refAppointmentComplete" gorm:"column:refAppointmentComplete"`
+	AppointmentPriority     string `json:"refAppointmentPriority" gorm:"column:refAppointmentPriority"`
+	AppointmentAccessId     int    `json:"refAppointmentAccessId" gorm:"column:refAppointmentAccessId"`
+	AppointmentAccessStatus bool   `json:"refAppointmentAccessStatus" gorm:"column:refAppointmentAccessStatus"`
+}
+
+type AnswerReportIntakeReq struct {
+	AppointmentId int    `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	QuestionId    int    `json:"questionId" binding:"required" mapstructure:"questionId"`
+	Answer        string `json:"answer" binding:"required" mapstructure:"answer"`
+	PatientId     int    `json:"patientId" binding:"required" mapstructure:"patientId"`
+}
+
+type AnswerTextContentReq struct {
+	AppointmentId int    `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	TextContent   string `json:"textContent" binding:"required" mapstructure:"textContent"`
+	PatientId     int    `json:"patientId" binding:"required" mapstructure:"patientId"`
+	SyncStatus    bool   `json:"syncStatus" binding:"required" mapstructure:"syncStatus"`
+}
+
+type GetReportTextContentModel struct {
+	RTCId   int    `json:"refRTCId" gorm:"column:refRTCId"`
+	RTCText string `json:"refRTCText" gorm:"column:refRTCText"`
+}
+
+type AddCommentReq struct {
+	AppointmentId int    `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	PatientId     int    `json:"patientId" binding:"required" mapstructure:"patientId"`
+	AssignId      int    `json:"assignto" binding:"required" mapstructure:"assignto"`
+	Status        string `json:"status" binding:"required" mapstructure:"status"`
+	Comments      string `json:"comments" binding:"required" mapstructure:"comments"`
+}
+
+type CompleteReportReq struct {
+	AppointmentId int    `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	PatientId     int    `json:"patientId" binding:"required" mapstructure:"patientId"`
+	MovedStatus   string `json:"movedStatus" binding:"required" mapstructure:"movedStatus"`
+	CurrentStatus string `json:"currentStatus" binding:"required" mapstructure:"currentStatus"`
+}
+
+type AnswerReqModel struct {
+	QuestionId int    `json:"questionId" binding:"required" mapstructure:"questionId"`
+	Answer     string `json:"answer" binding:"required" mapstructure:"answer"`
+}
+
+type SubmitReportReq struct {
+	ReportIntakeForm     []AnswerReqModel `json:"reportIntakeForm" binding:"required" mapstructure:"reportIntakeForm"`
+	TechnicianIntakeForm []AnswerReqModel `json:"technicianIntakeForm" binding:"required" mapstructure:"technicianIntakeForm"`
+	PatientIntakeForm    []AnswerReqModel `json:"patientIntakeForm" binding:"required" mapstructure:"patientIntakeForm"`
+	ReportTextContent    string           `json:"reportTextContent" binding:"required" mapstructure:"reportTextContent"`
+	AppointmentId        int              `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	PatientId            int              `json:"patientId" binding:"required" mapstructure:"patientId"`
+	MovedStatus          string           `json:"movedStatus" binding:"required" mapstructure:"movedStatus"`
+	CurrentStatus        string           `json:"currentStatus" binding:"required" mapstructure:"currentStatus"`
+	SyncStatus           bool             `json:"syncStatus" binding:"required" mapstructure:"syncStatus"`
+}
+
+type UpdateRemarkReq struct {
+	AppointmentId int    `json:"appointmentId" binding:"required" mapstructure:"appointmentId"`
+	PatientId     int    `json:"patientId" binding:"required" mapstructure:"patientId"`
+	Remark        string `json:"remark" binding:"required" mapstructure:"remark"`
+}
+
+type UploadReportFormateReq struct {
+	Name            string `json:"name" binding:"required" mapstructure:"name"`
+	FormateTemplate string `json:"formateTemplate" binding:"required" mapstructure:"formateTemplate"`
+}
+
+type GetUserDetails struct {
+	CustId         string `json:"refUserCustId" gorm:"column:refUserCustId"`
+	FirstName      string `json:"refUserFirstName" gorm:"column:refUserFirstName"`
+	Email          string `json:"refCODOEmail" gorm:"column:refCODOEmail"`
+	Specialization string `json:"specialization" gorm:"column:specialization"`
+	Department     string `json:"department" gorm:"column:department"`
+}
+
+type GetReportFormateReq struct {
+	Id int `json:"id" binding:"required" mapstructure:"id"`
+}
+
+type ReportFormateModel struct {
+	RFId        int       `json:"refRFId" gorm:"column:refRFId"`
+	RFName      string    `json:"refRFName" gorm:"column:refRFName"`
+	RFCreatedAt time.Time `json:"refRFCreatedAt" gorm:"column:refRFCreatedAt"`
+	RFCreatedBy int       `json:"refRFCreatedBy" gorm:"column:refRFCreatedBy"`
+}
+
+type ReportTextFormateModel struct {
+	RFId        int       `json:"refRFId" gorm:"column:refRFId"`
+	RFName      string    `json:"refRFName" gorm:"column:refRFName"`
+	RFText      string    `json:"refRFText" gorm:"column:refRFText"`
+	RFCreatedAt time.Time `json:"refRFCreatedAt" gorm:"column:refRFCreatedAt"`
+	RFCreatedBy int       `json:"refRFCreatedBy" gorm:"column:refRFCreatedBy"`
+}
+
+type GetOneUserAppointmentModel struct {
+	AppointmentId             int    `json:"refAppointmentId" gorm:"column:refAppointmentId"`
+	UserId                    int    `json:"refUserId" gorm:"column:refUserId"`
+	SCId                      int    `json:"refSCId" gorm:"column:refSCId"`
+	SCCustId                  string `json:"refSCCustId" gorm:"column:refSCCustId"`
+	SCName                    string `json:"refSCName" gorm:"column:refSCName"`
+	CategoryId                int    `json:"refCategoryId" gorm:"column:refCategoryId"`
+	AppointmentDate           string `json:"refAppointmentDate" gorm:"column:refAppointmentDate"`
+	AppointmentComplete       string `json:"refAppointmentComplete" gorm:"column:refAppointmentComplete"`
+	AppointmentPriority       string `json:"refAppointmentPriority" gorm:"column:refAppointmentPriority"`
+	AppointmentAccessId       int    `json:"refAppointmentAccessId" gorm:"column:refAppointmentAccessId"`
+	AppointmentAccessStatus   bool   `json:"refAppointmentAccessStatus" gorm:"column:refAppointmentAccessStatus"`
+	AppointmentAssignedUserId int    `json:"refAppointmentAssignedUserId" gorm:"column:refAppointmentAssignedUserId"`
+	AppointmentRemarks        string `json:"refAppointmentRemarks" gorm:"column:refAppointmentRemarks"`
+}
+
+type PatientCustId struct {
+	CustId string `json:"refUserCustId" gorm:"column:refUserCustId"`
+}
---
+++ query/Appointment/ReportIntakeFormQuery.go
@@ -0,0 +1,365 @@
+package query
+
+var CheckAccessSQL = `
+SELECT
+  CASE 
+    WHEN "refAppointmentAccessStatus" = true 
+         AND "refAppointmentAccessId" != ? THEN false
+    ELSE true
+  END AS "status",
+  "refAppointmentAccessId"
+FROM
+  appointment."refAppointments"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var GetOneUserAppointment = `
+SELECT
+  sc."refSCCustId",
+  sc."refSCName",
+  ra.*
+FROM
+  appointment."refAppointments" ra
+  JOIN public."ScanCenter" sc ON sc."refSCId" = ra."refSCId"
+WHERE
+  ra."refUserId" = ?
+  AND ra."refAppointmentId" = ?
+`
+
+var GetIntakeFormSQL = `
+SELECT
+  *
+FROM
+  notes."refIntakeForm"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var GetTechnicianIntakeFormSQL = `
+SELECT
+  *
+FROM
+  notes."refTechnicianIntakeForm"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var GetReportIntakeFormSQL = `
+SELECT
+  *
+FROM
+  notes."refReportIntakeForm"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var GetReporttextContent = `
+SELECT
+  *
+FROM
+  notes."refReportsTextContent"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var GetReportHistorySQL = `
+SELECT
+  f."refUserFirstName" AS "HandleUserName",
+  rrh.*
+FROM
+  notes."refReportsHistory" rrh
+  JOIN public."Users" f ON f."refUserId" = rrh."refRHHandledUserId"
+WHERE
+  rrh."refAppointmentId" = ?
+ORDER BY
+  rrh."refRHId" ASC
+`
+
+var GetReportCommentsSQL = `
+SELECT
+  f."refUserFirstName" AS "UserForName",
+  b."refUserFirstName" AS "UserByName",
+  rrc.*
+FROM
+  notes."refReportsComments" rrc
+  JOIN public."Users" f ON f."refUserId" = rrc."refRCFor"
+  JOIN public."Users" b ON b."refUserId" = rrc."refRCBy"
+WHERE
+  rrc."refAppointmentId" = ?
+`
+
+var GetAppointmentSQL = `
+SELECT
+  *
+FROM
+  appointment."refAppointments"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var UpdateAccessAppointment = `
+UPDATE
+  appointment."refAppointments"
+SET
+  "refAppointmentAccessStatus" = ?,
+  "refAppointmentAccessId" = ?
+WHERE
+  "refAppointmentId" = ?
+`
+
+var GetReportIntakeFormQuestionSQL = `
+SELECT
+  *
+FROM
+  notes."refReportIntakeForm"
+WHERE
+  "refAppointmentId" = ?
+  AND "refRITFQId" = ?
+`
+
+var GetTechnicianIntakeFormQuestionSQL = `
+SELECT
+  *
+FROM
+  notes."refTechnicianIntakeForm"
+WHERE
+  "refAppointmentId" = ?
+  AND "refTITFQId" = ?
+`
+
+var GetPatientIntakeFormQuestionSQL = `
+SELECT
+  *
+FROM
+  notes."refIntakeForm"
+WHERE
+  "refAppointmentId" = ?
+  AND "refITFQId" = ?
+`
+
+var InsertTechnicianIntakeSQL = `
+INSERT INTO
+  notes."refReportIntakeForm" (
+    "refUserId",
+    "refAppointmentId",
+    "refRITFQId",
+    "refRITFAnswer",
+    "refRITFCreatedAt",
+    "refRITFCreatedBy"
+  )
+VALUES
+  (?, ?, ?, ?, NOW(), ?);
+`
+
+var UpdateReportIntakeSQL = `
+UPDATE
+  notes."refReportIntakeForm"
+SET
+  "refRITFAnswer" = ?,
+  "refRITFUpdatedAt" = NOW(),
+  "refRITFUpdatedBy" = ?
+WHERE
+  "refRITFQId" = ?
+  AND "refAppointmentId" = ?
+`
+
+var UpdateTechnicianIntakeSQL = `
+UPDATE
+  notes."refTechnicianIntakeForm"
+SET
+  "refTITFAnswer" = ?,
+  "refTITFUpdatedAt" = NOW(),
+  "refTITFUpdatedBy" = ?
+WHERE
+  "refTITFQId" = ?
+  AND "refAppointmentId" = ?
+`
+
+var UpdatePatientIntakeSQL = `
+UPDATE
+  notes."refIntakeForm"
+SET
+  "refITFAnswer" = ?,
+  "refITFUpdatedAt" = NOW(),
+  "refITFUpdatedBy" = ?
+WHERE
+  "refITFQId" = ?
+  AND "refAppointmentId" = ?
+`
+
+var GetTextContentSQL = `
+SELECT
+  *
+FROM
+  notes."refReportsTextContent"
+WHERE
+  "refAppointmentId" = ?
+`
+
+var InsertTextContentSQL = `
+INSERT INTO
+  notes."refReportsTextContent" (
+    "refUserId",
+    "refAppointmentId",
+    "refRTCText",
+    "refRTCreatedAt",
+    "refRTCreatedBy",
+    "refRTSyncStatus"
+  )
+VALUES
+  (?, ?, ?, NOW(), ?, ?);
+`
+
+var UpdateTextContentSQL = `
+UPDATE
+  notes."refReportsTextContent"
+SET
+  "refRTCText" = ?,
+  "refRTUpdatedAt" = NOW(),
+  "refRTUpdatedBy" = ?,
+  "refRTSyncStatus" = ?
+WHERE
+  "refAppointmentId" = ?
+`
+
+var InsertCommentsSQL = `
+INSERT INTO
+  notes."refReportsComments" (
+    "refUserId",
+    "refAppointmentId",
+    "refRCFor",
+    "refRCBy",
+    "refRCStatus",
+    "refRCComments",
+    "refRCCreatedAt"
+  )
+VALUES
+  (?, ?, ?, ?, ?, ?, NOW());
+`
+
+var UpdateReportRemarksSQL = `
+UPDATE
+  appointment."refAppointments"
+SET
+  "refAppointmentRemarks" = ?
+WHERE
+  "refAppointmentId" = ?
+  AND "refUserId" = ?
+`
+
+var InsertReportHistorySQL = `
+INSERT INTO
+  notes."refReportsHistory" (
+    "refUserId",
+    "refAppointmentId",
+    "refRHHandledUserId",
+    "refRHHandleStartTime"
+  )
+VALUES
+  (?, ?, ?, NOW());
+`
+
+var CompleteReportAppointmentSQL = `
+UPDATE
+  appointment."refAppointments"
+SET
+  "refAppointmentComplete" = ?,
+  "refAppointmentAccessStatus" = false,
+  "refAppointmentAccessId" = NULL
+WHERE
+  "refAppointmentId" = ?
+  AND "refUserId" = ?
+`
+
+var CompleteReportHistorySQL = `
+UPDATE notes."refReportsHistory"
+SET
+  "refRHHandleEndTime" = NOW(),
+  "refRHHandleStatus" = ?,
+  "refRHHandleContentText" = ?
+WHERE "refRHId" = (
+  SELECT MAX("refRHId")
+  FROM notes."refReportsHistory"
+  WHERE
+    "refAppointmentId" = ?
+    AND "refRHHandledUserId" = ?
+    AND "refUserId" = ?
+)
+`
+
+var InsertReportTemplate = `
+INSERT INTO
+  notes."refReportFormate" (
+    "refRFName",
+    "refRFText",
+    "refRFCreatedAt",
+    "refRFCreatedBy",
+    "refRFStatus"
+  )
+VALUES
+  (?, ?, NOW(), ?, true)
+  RETURNING "refRFId"
+`
+
+var GetReportFormateListSQL = `
+SELECT
+  *
+FROM
+  notes."refReportFormate"
+WHERE
+  "refRFStatus" = true
+`
+
+var GetOneReportFormateListSQL = `
+SELECT
+  *
+FROM
+  notes."refReportFormate"
+WHERE
+  "refRFStatus" = true
+  AND "refRFId" = ?
+`
+
+var GetUserDetailsSQL = `
+SELECT
+  u."refUserCustId",
+  u."refUserId",
+  u."refUserFirstName",
+  rcd."refCODOEmail",
+  u."refRTId",
+  COALESCE(
+    dd."refDDSpecialization",
+    rad."refRASpecialization",
+    cod."refCDSpecialization"
+  ) AS "specialization",
+  COALESCE(sc."refSCName", 'Wellthgreen HealthCare') AS "department"
+FROM
+  public."Users" u
+  LEFT JOIN userdomain."refTechnicianDomain" td ON u."refRTId" = 2
+  AND td."refUserId" = u."refUserId"
+  LEFT JOIN userdomain."refReceptionstDomain" rd ON u."refRTId" = 3
+  AND rd."refUserId" = u."refUserId"
+  LEFT JOIN userdomain."refDoctorDomain" dd ON u."refRTId" = 5
+  AND dd."refUserId" = u."refUserId"
+  LEFT JOIN userdomain."refRadiologistDomain" rad ON u."refRTId" = 6
+  AND rad."refUserId" = u."refUserId"
+  LEFT JOIN userdomain."refScribeDomain" sd ON u."refRTId" = 7
+  AND sd."refUserId" = u."refUserId"
+  LEFT JOIN userdomain."refCoDoctorDomain" cod ON u."refRTId" = 8
+  AND cod."refUserId" = u."refUserId"
+  JOIN userdomain."refCommunicationDomain" rcd ON rcd."refUserId" = u."refUserId"
+  LEFT JOIN map."refScanCenterMap" rscm ON rscm."refUserId" = u."refUserId"
+  LEFT JOIN public."ScanCenter" sc ON sc."refSCId" = rscm."refSCId"
+WHERE
+  u."refUserId" = $1;
+`
+
+var PatientUserDetailsSQL = `
+SELECT
+  *
+FROM
+  public."Users"
+WHERE
+  "refUserId" = ?
+`
