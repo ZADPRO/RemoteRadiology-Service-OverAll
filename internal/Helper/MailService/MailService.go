315e52d6fe0457008eb683475241176426cf390d
gokulmzadroit <gokul.m@zadroit.com>
2025-09-23 23:17:46 +0530

    Updated the Mail Fixes

--- internal/Helper/MailService/MailService.go
+++ internal/Helper/MailService/MailService.go
@@ -1,26 +1,26 @@
 package mailservice
 
 import (
-	"fmt"
-	"log"
+	logger "AuthenticationService/internal/Helper/Logger"
 	"os"
 
 	"gopkg.in/gomail.v2"
 )
 
 func MailService(toMailer string, htmlContent string, subject string) bool {
+	log := logger.InitLogger()
 	m := gomail.NewMessage()
 	m.SetHeader("From", os.Getenv("EMAILID"))
 	m.SetHeader("To", toMailer)
 	m.SetHeader("Subject", subject)
 	m.SetBody("text/html", htmlContent)
 
-	fmt.Println(os.Getenv("PASSWORD"))
+	log.Error(os.Getenv("PASSWORD"))
 
 	d := gomail.NewDialer("smtpout.secureserver.net", 465, os.Getenv("EMAILID"), os.Getenv("PASSWORD"))
 
 	if err := d.DialAndSend(m); err != nil {
-		log.Printf("Could not send email: %v", err)
+		log.Errorf("Could not send email: %v", err)
 		return false
 	}
 
