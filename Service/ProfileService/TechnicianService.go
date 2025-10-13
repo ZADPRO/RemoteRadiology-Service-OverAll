ebf14f0b49b1847da871f9b223ddf04ae791c4e0
ionhax-dev <thirukumara.d@zadroit.com>
2025-10-13 08:18:08 +0530

    service bugs fixed

--- Service/ProfileService/TechnicianService.go
+++ Service/ProfileService/TechnicianService.go
@@ -1,13 +1,17 @@
 package service
 
 import (
+	s3Service "AuthenticationService/Service/S3"
 	hashdb "AuthenticationService/internal/Helper/HashDB"
 	logger "AuthenticationService/internal/Helper/Logger"
 	helper "AuthenticationService/internal/Helper/ViewFile"
 	model "AuthenticationService/internal/Model/ProfileService"
 	query "AuthenticationService/query/ProfileService"
+	"context"
+	"time"
 
 	"gorm.io/gorm"
+
 )
 
 func GetAllTechnicianDataService(db *gorm.DB, reqVal model.GetReceptionistReq) []model.GetAllRadiologist {
@@ -58,6 +62,96 @@ func GetOneTechnicianDataService(db *gorm.DB, reqVal model.GetOneReceptionistReq
 		return []model.GetTechnicianOne{}
 	}
 
+	// for i, tech := range RadiologistData {
+	// 	RadiologistData[i].FirstName = hashdb.Decrypt(tech.FirstName)
+	// 	RadiologistData[i].LastName = hashdb.Decrypt(tech.LastName)
+	// 	RadiologistData[i].ProfileImg = hashdb.Decrypt(tech.ProfileImg)
+	// 	RadiologistData[i].DOB = hashdb.Decrypt(tech.DOB)
+	// 	RadiologistData[i].SSNo = hashdb.Decrypt(tech.SSNo)
+	// 	RadiologistData[i].DrivingLicense = hashdb.Decrypt(tech.DrivingLicense)
+	// 	RadiologistData[i].DigitalSignature = hashdb.Decrypt(tech.DigitalSignature)
+
+	// 	if len(hashdb.Decrypt(tech.ProfileImg)) > 0 {
+	// 		profileImgHelperData, viewErr := helper.ViewFile("./Assets/Profile/" + hashdb.Decrypt(tech.ProfileImg))
+	// 		if viewErr != nil {
+	// 			// Consider if Fatalf is appropriate or if logging a warning and setting to nil is better
+	// 			log.Errorf("Failed to read profile image file: %v", viewErr)
+	// 		}
+	// 		if profileImgHelperData != nil {
+	// 			RadiologistData[i].ProfileImgFile = &model.FileData{
+	// 				Base64Data:  profileImgHelperData.Base64Data,
+	// 				ContentType: profileImgHelperData.ContentType,
+	// 			}
+	// 		}
+	// 	} else {
+	// 		RadiologistData[i].ProfileImgFile = nil
+	// 	}
+
+	// 	var databaseLicenseFiles []model.LicenseFilesModel
+	// 	LicenseErr := db.Raw(query.GetLicenseFilesSQL, reqVal.UserId).Scan(&databaseLicenseFiles).Error
+	// 	if LicenseErr != nil {
+	// 		log.Printf("ERROR: Failed to fetch License files for user ID %d: %v", reqVal.UserId, LicenseErr)
+	// 		return []model.GetTechnicianOne{}
+	// 	}
+
+	// 	if len(databaseLicenseFiles) > 0 {
+	// 		RadiologistData[i].LicenseFiles = make([]model.LicenseFilesModel, 0, len(databaseLicenseFiles))
+	// 		for _, dbLicenseItem := range databaseLicenseFiles {
+	// 			processedLicenseItem := model.LicenseFilesModel{
+	// 				LId:          dbLicenseItem.LId,
+	// 				LFileName:    hashdb.Decrypt(dbLicenseItem.LFileName),
+	// 				LOldFileName: hashdb.Decrypt(dbLicenseItem.LOldFileName),
+	// 			}
+	// 			licenseHelperFileData, licenseFileReadErr := helper.ViewFile("./Assets/Files/" + processedLicenseItem.LFileName)
+	// 			if licenseFileReadErr != nil {
+	// 				log.Printf("WARNING: Failed to read License file %s: %v. Skipping file data.", processedLicenseItem.LFileName, licenseFileReadErr)
+	// 				processedLicenseItem.LFileData = nil
+	// 			} else if licenseHelperFileData != nil {
+	// 				processedLicenseItem.LFileData = &model.FileData{
+	// 					Base64Data:  licenseHelperFileData.Base64Data,
+	// 					ContentType: licenseHelperFileData.ContentType,
+	// 				}
+	// 			} else {
+	// 				processedLicenseItem.LFileData = nil // Should ideally not happen if error is nil
+	// 			}
+	// 			RadiologistData[i].LicenseFiles = append(RadiologistData[i].LicenseFiles, processedLicenseItem)
+	// 		}
+	// 	}
+
+	// 	if len(hashdb.Decrypt(tech.DrivingLicense)) > 0 {
+	// 		DriversLicenseNoImgHelperData, viewErr := helper.ViewFile("./Assets/Files/" + hashdb.Decrypt(tech.DrivingLicense))
+	// 		if viewErr != nil {
+	// 			// Consider if Fatalf is appropriate or if logging a warning and setting to nil is better
+	// 			log.Errorf("Failed to read DrivingLicense file: %v", viewErr)
+	// 		}
+	// 		if DriversLicenseNoImgHelperData != nil {
+	// 			RadiologistData[i].DrivingLicenseFile = &model.FileData{
+	// 				Base64Data:  DriversLicenseNoImgHelperData.Base64Data,
+	// 				ContentType: DriversLicenseNoImgHelperData.ContentType,
+	// 			}
+	// 		}
+	// 	} else {
+	// 		RadiologistData[i].DrivingLicenseFile = nil
+	// 	}
+
+	// 	if len(hashdb.Decrypt(tech.DigitalSignature)) > 0 {
+	// 		DigitalSignatureHelper, viewErr := helper.ViewFile("./Assets/Profile/" + hashdb.Decrypt(tech.DigitalSignature))
+	// 		if viewErr != nil {
+	// 			// Consider if Fatalf is appropriate or if logging a warning and setting to nil is better
+	// 			log.Errorf("Failed to read DigitalSignature file: %v", viewErr)
+	// 		}
+	// 		if DigitalSignatureHelper != nil {
+	// 			RadiologistData[i].DigitalSignatureFile = &model.FileData{
+	// 				Base64Data:  DigitalSignatureHelper.Base64Data,
+	// 				ContentType: DigitalSignatureHelper.ContentType,
+	// 			}
+	// 		}
+	// 	} else {
+	// 		RadiologistData[i].DigitalSignatureFile = nil
+	// 	}
+
+	// }
+
 	for i, tech := range RadiologistData {
 		RadiologistData[i].FirstName = hashdb.Decrypt(tech.FirstName)
 		RadiologistData[i].LastName = hashdb.Decrypt(tech.LastName)
@@ -67,13 +161,12 @@ func GetOneTechnicianDataService(db *gorm.DB, reqVal model.GetOneReceptionistReq
 		RadiologistData[i].DrivingLicense = hashdb.Decrypt(tech.DrivingLicense)
 		RadiologistData[i].DigitalSignature = hashdb.Decrypt(tech.DigitalSignature)
 
-		if len(hashdb.Decrypt(tech.ProfileImg)) > 0 {
-			profileImgHelperData, viewErr := helper.ViewFile("./Assets/Profile/" + hashdb.Decrypt(tech.ProfileImg))
+		// ==================== PROFILE IMAGE (LOCAL) ====================
+		if len(RadiologistData[i].ProfileImg) > 0 {
+			profileImgHelperData, viewErr := helper.ViewFile("./Assets/Profile/" + RadiologistData[i].ProfileImg)
 			if viewErr != nil {
-				// Consider if Fatalf is appropriate or if logging a warning and setting to nil is better
 				log.Errorf("Failed to read profile image file: %v", viewErr)
-			}
-			if profileImgHelperData != nil {
+			} else if profileImgHelperData != nil {
 				RadiologistData[i].ProfileImgFile = &model.FileData{
 					Base64Data:  profileImgHelperData.Base64Data,
 					ContentType: profileImgHelperData.ContentType,
@@ -83,6 +176,7 @@ func GetOneTechnicianDataService(db *gorm.DB, reqVal model.GetOneReceptionistReq
 			RadiologistData[i].ProfileImgFile = nil
 		}
 
+		// ==================== LICENSE FILES ====================
 		var databaseLicenseFiles []model.LicenseFilesModel
 		LicenseErr := db.Raw(query.GetLicenseFilesSQL, reqVal.UserId).Scan(&databaseLicenseFiles).Error
 		if LicenseErr != nil {
@@ -98,29 +192,50 @@ func GetOneTechnicianDataService(db *gorm.DB, reqVal model.GetOneReceptionistReq
 					LFileName:    hashdb.Decrypt(dbLicenseItem.LFileName),
 					LOldFileName: hashdb.Decrypt(dbLicenseItem.LOldFileName),
 				}
-				licenseHelperFileData, licenseFileReadErr := helper.ViewFile("./Assets/Files/" + processedLicenseItem.LFileName)
-				if licenseFileReadErr != nil {
-					log.Printf("WARNING: Failed to read License file %s: %v. Skipping file data.", processedLicenseItem.LFileName, licenseFileReadErr)
-					processedLicenseItem.LFileData = nil
-				} else if licenseHelperFileData != nil {
-					processedLicenseItem.LFileData = &model.FileData{
-						Base64Data:  licenseHelperFileData.Base64Data,
-						ContentType: licenseHelperFileData.ContentType,
+
+				// ✅ If file stored in S3 → generate presigned URL
+				if isS3URL(processedLicenseItem.LFileName) {
+					key := extractS3Key(processedLicenseItem.LFileName)
+					presignedURL, err := s3Service.GeneratePresignGetURL(context.Background(), key, 10*time.Minute)
+					if err != nil {
+						log.Printf("WARNING: Failed to presign License file %s: %v", processedLicenseItem.LFileName, err)
+					} else {
+						processedLicenseItem.LFileName = presignedURL
 					}
+					processedLicenseItem.LFileData = nil // since file is remote
 				} else {
-					processedLicenseItem.LFileData = nil // Should ideally not happen if error is nil
+					// ✅ Local file fallback (existing logic)
+					licenseHelperFileData, licenseFileReadErr := helper.ViewFile("./Assets/Files/" + processedLicenseItem.LFileName)
+					if licenseFileReadErr != nil {
+						log.Printf("WARNING: Failed to read License file %s: %v. Skipping file data.", processedLicenseItem.LFileName, licenseFileReadErr)
+						processedLicenseItem.LFileData = nil
+					} else if licenseHelperFileData != nil {
+						processedLicenseItem.LFileData = &model.FileData{
+							Base64Data:  licenseHelperFileData.Base64Data,
+							ContentType: licenseHelperFileData.ContentType,
+						}
+					}
 				}
+
 				RadiologistData[i].LicenseFiles = append(RadiologistData[i].LicenseFiles, processedLicenseItem)
 			}
 		}
 
-		if len(hashdb.Decrypt(tech.DrivingLicense)) > 0 {
-			DriversLicenseNoImgHelperData, viewErr := helper.ViewFile("./Assets/Files/" + hashdb.Decrypt(tech.DrivingLicense))
-			if viewErr != nil {
-				// Consider if Fatalf is appropriate or if logging a warning and setting to nil is better
-				log.Errorf("Failed to read DrivingLicense file: %v", viewErr)
+		// ==================== DRIVING LICENSE ====================
+		if isS3URL(RadiologistData[i].DrivingLicense) {
+			key := extractS3Key(RadiologistData[i].DrivingLicense)
+			presignedURL, err := s3Service.GeneratePresignGetURL(context.Background(), key, 10*time.Minute)
+			if err == nil {
+				RadiologistData[i].DrivingLicense = presignedURL
+				RadiologistData[i].DrivingLicenseFile = nil
+			} else {
+				log.Errorf("Failed to generate presigned URL for Driving License: %v", err)
 			}
-			if DriversLicenseNoImgHelperData != nil {
+		} else if len(RadiologistData[i].DrivingLicense) > 0 {
+			DriversLicenseNoImgHelperData, viewErr := helper.ViewFile("./Assets/Files/" + RadiologistData[i].DrivingLicense)
+			if viewErr != nil {
+				log.Errorf("Failed to read DrivingLicense file: %v", viewErr)
+			} else if DriversLicenseNoImgHelperData != nil {
 				RadiologistData[i].DrivingLicenseFile = &model.FileData{
 					Base64Data:  DriversLicenseNoImgHelperData.Base64Data,
 					ContentType: DriversLicenseNoImgHelperData.ContentType,
@@ -130,13 +245,12 @@ func GetOneTechnicianDataService(db *gorm.DB, reqVal model.GetOneReceptionistReq
 			RadiologistData[i].DrivingLicenseFile = nil
 		}
 
-		if len(hashdb.Decrypt(tech.DigitalSignature)) > 0 {
-			DigitalSignatureHelper, viewErr := helper.ViewFile("./Assets/Profile/" + hashdb.Decrypt(tech.DigitalSignature))
+		// ==================== DIGITAL SIGNATURE (LOCAL) ====================
+		if len(RadiologistData[i].DigitalSignature) > 0 {
+			DigitalSignatureHelper, viewErr := helper.ViewFile("./Assets/Profile/" + RadiologistData[i].DigitalSignature)
 			if viewErr != nil {
-				// Consider if Fatalf is appropriate or if logging a warning and setting to nil is better
 				log.Errorf("Failed to read DigitalSignature file: %v", viewErr)
-			}
-			if DigitalSignatureHelper != nil {
+			} else if DigitalSignatureHelper != nil {
 				RadiologistData[i].DigitalSignatureFile = &model.FileData{
 					Base64Data:  DigitalSignatureHelper.Base64Data,
 					ContentType: DigitalSignatureHelper.ContentType,
@@ -145,7 +259,6 @@ func GetOneTechnicianDataService(db *gorm.DB, reqVal model.GetOneReceptionistReq
 		} else {
 			RadiologistData[i].DigitalSignatureFile = nil
 		}
-
 	}
 
 	return RadiologistData
